9342f1586cd53ba69e2f1a68eff0b91b0acf35ce
Rajesh Mondal <rajeshmondal9007@gmail.com>
2024-03-05 22:51:29 +0530

    Rajesh: Added Debug Log

--- scraping/views.py
+++ scraping/views.py
@@ -16,6 +16,7 @@ def index(request):
         url = request.POST.get("website")
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
         response = requests.get(url, headers=headers)
+        response.raise_for_status()
         products = []
         print("**** Response ***")
         print(response)
